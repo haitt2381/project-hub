00ec850f185d50c449983a0da73ba29979483f82
HaiTT <haitt2381@gmail.com>
2023-12-21 13:57:52 +0700

    Grafbase setup test version

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,4 +1,6 @@
-import { g, config } from '@grafbase/sdk'
+import { graph, config, connector } from '@grafbase/sdk'
+
+const g = graph.Standalone()
 
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
@@ -20,5 +22,5 @@ const Project = g.model('Project', {
 })
 
 export default config({
-  schema: g
+  graph: g,
 })
